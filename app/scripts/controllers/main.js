0ad7e6aa4d80c3fc8222a5f267fda8ccadefd3f3
testjo <1island@gmail.com>
2016-02-07 08:35:06 -0500

    tst

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name moltinApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the moltinApp
+ */
+angular.module('moltinApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
